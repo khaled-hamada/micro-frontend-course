3c7512a6228b47e8a19ba0e631aaf4e0371a54bf
khaled hamada <khamada@alpha-sense.com>
2023-02-04 16:29:31 +0200

    initial commit

---
+++ container/config/webpack.common.js
@@ -0,0 +1,19 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // enable processing react jsx code, and other js versions
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            // enable some features like async await syn.
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
