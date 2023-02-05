167ae4a8c6514c25b4bfd18a1311b59ca6a9b4bb
khaled hamada <khamada@alpha-sense.com>
2023-02-05 16:40:30 +0200

    fix css scoping issues

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container-',
+});
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
