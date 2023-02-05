2f60cba223ab6a818db0136ec4c271473a430c5e
khaled hamada <khamada@alpha-sense.com>
2023-02-05 16:43:44 +0200

    fix css scoping issues

--- container/src/App.js
+++ container/src/App.js
@@ -8,14 +8,14 @@ const generateClassName = createGenerateClassName({
 });
 const App = () => {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
         </div>
-      </BrowserRouter>
-    </StylesProvider>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
 
