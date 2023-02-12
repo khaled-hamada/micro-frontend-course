ec83ee4f57bcacf45ba5c311e0530ad9feb82b9e
khaled hamada <khamada@alpha-sense.com>
2023-02-12 15:16:59 +0200

    add dashboard app access restriction

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,10 @@
-import React, { lazy, Suspense } from 'react';
-// import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import Header from './components/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core';
-// import AuthApp from './components/AuthApp';
 import Progress from './components/Progress';
-import { useState } from 'react';
+import { createBrowserHistory } from 'history';
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'container-',
 });
@@ -13,11 +12,19 @@ const generateClassName = createGenerateClassName({
 const MarketingAppLazy = lazy(() => import('./components/MarketingApp'));
 const AuthAppLazy = lazy(() => import('./components/AuthApp'));
 const DashboardLazy = lazy(() => import('./components/Dashboard'));
+const history = createBrowserHistory();
 
 const App = () => {
   const [isSignedIn, setSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -29,13 +36,15 @@ const App = () => {
               <Route path="/auth">
                 <AuthAppLazy onSignIn={() => setSignedIn(true)} />
               </Route>
-              <Route path="/dashboard" component={DashboardLazy} />
+              <Route path="/dashboard">
+                {isSignedIn ? <DashboardLazy /> : <Redirect to="/" />}
+              </Route>
               <Route path="/" component={MarketingAppLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
